c91f8eb50808a6c0fc637dbc85d7170186140077
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-10-24 10:36:38 +0300

    Форма пошуку

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import axios from 'axios';
+import { Searchbar } from './Searchbar/Searchbar';
+// import PropTypes from 'prop-types';
+
+const API_KEY = `29781488-c1f8f32b8cf0d06ff300c84b0`;
+const OPTIONS = `image_type=photo&orientation=horizontal&safesearch=true&lang=en&lang=uk&per_page=12`;
+
+export class App extends React.Component {
+  state = {
+    page: 1,
+    totalPage: null,
+    searchQueryInput: '',
+    hits: [],
+    status: 'idle',
+  }
+  componentDidMount() {
+    axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${this.state.searchQueryInput}&${OPTIONS}&page=${this.state.page}`)
+      
+    .then(res => {
+      const hits = res.data;
+      this.setState({ hits });
+      console.log(hits)
+    })
+  }
+
+  handleFormSubmit = searchQueryInput => {
+    this.setState({ searchQueryInput });
+  };
+
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleFormSubmit}/>
+        <ul className="gallery">
+          {/* { this.state.hits.map(hit => <li className="gallery-item">
+            <img src="{webformatURL}" alt="{tegs}" />
+          </li>)} */}
+        </ul>
+      </div>
+    )
+  }
+}
+
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {ImSearch} from 'react-icons/im'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import PropTypes from 'prop-types';
+
+export class Searchbar extends React.Component {
+    state = {
+        searchQueryInput: '',
+    };
+
+    handleNameChange = event => {
+        this.setState({ searchQueryInput: event.currentTarget.value.toLowerCase() });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        if (this.state.searchQueryInput.trim() === '') {
+          return Notify.failure('Enter your query, please');
+        }
+        this.props.onSubmit(this.state.searchQueryInput);
+        this.setState({ searchQueryInput: '' });
+    };
+
+    render() {
+        return (
+            <header className="searchbar">
+            <form className="form" onSubmit={this.handleSubmit}>
+                <button type="submit" className="button">
+                <ImSearch className="button-label" />
+                </button>
+
+                <input
+                    onChange={this.handleNameChange}
+                    value={this.searchQueryInput}
+                    className="input"
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                />
+            </form>
+            </header>
+        )
+    }
+}
+
+Searchbar.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
